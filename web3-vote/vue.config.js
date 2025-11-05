64722eac18dbafdfbb14559d9b78e72e556dc2f8
gaohan0121 <1448802330@qq.com>
2025-11-06 02:00:02 +0800

    Save vue.config.js and dist before switching to gh-pages

--- web3-vote/vue.config.js
+++ web3-vote/vue.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 const { VantResolver } = require("unplugin-vue-components/resolvers");
 const ComponentsPlugin = require("unplugin-vue-components/webpack");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './', // ✅ 保证构建资源使用相对路径，防止 GitHub Pages 白屏
   configureWebpack: {
     plugins: [
       ComponentsPlugin({
